8eb7e0d6204438ff089e640a473a9e09bb148426
Katie Owen <katherinehowen@gmail.com>
2016-03-03 00:25:00 -0500

    Routers for students are working.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,28 +4,67 @@
 
 class MainController {
 
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
+  // constructor($http) {
+  //   this.$http = $http;
+  //   this.awesomeThings = [];
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
+  //   $http.get('/api/things').then(response => {
+  //     this.awesomeThings = response.data;
+  //   });
+  // }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
+  // addThing() {
+  //   if (this.newThing) {
+  //     this.$http.post('/api/things', { name: this.newThing });
+  //     this.newThing = '';
+  //   }
+  // }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  // deleteThing(thing) {
+  //   this.$http.delete('/api/things/' + thing._id);
+  // }
 }
 
 angular.module('medoApp')
   .controller('MainController', MainController);
+
+StudentsController.$inject = ['$http'];
+
+function StudentsController($http){
+  var self = this;
+  self.all = [];
+  self.addStudent= addStudent;
+  self.newStudent= {};
+  self.getStudentss = getStudents;
+  self.deleteStudent= deleteStudent;
+
+  getStudents();
+  function getStudents(){
+    $http
+      .get('http://localhost:3000/students')
+      .then(function(response){
+        self.all = response.data.students;
+    });
+  }
+
+  function addStudent(){
+    $http
+      .post('http://localhost:3000/students', self.newStudent)
+      .then(function(response){
+        getStudents();
+    });
+    self.newStudent= {};
+  }
+
+  function deleteStudent(student){
+    $http
+      .delete("http://localhost:3000/students/" + student._id)
+      .then(function(response){
+        var index = self.all.indexOf(student);
+        self.all.splice(index, 1);
+      });
+  }
+
+}
 
 })();
--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,5 +8,29 @@ angular.module('medoApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('index', {
+        url: '/students',
+        templateUrl: 'app/main/list.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('new', {
+        url: '/new',
+        templateUrl: 'app/main/new.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/main/about.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      })
+      .state('show', {
+        url: '/students/:id',
+        templateUrl: 'app/main/show.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
   });
